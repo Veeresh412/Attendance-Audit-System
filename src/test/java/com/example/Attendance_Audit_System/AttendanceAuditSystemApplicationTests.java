851b15ef9f508f42ba35f5c0dc270f796db9f8b2
Tony Stark <vbhat2006@gmail.com>
2026-06-02 12:41:22 +0530

    spring files added

---
+++ src/test/java/com/example/Attendance_Audit_System/AttendanceAuditSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Attendance_Audit_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceAuditSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
