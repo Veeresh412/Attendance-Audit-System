57968d51c9fa976e842aafda200f95a795fe3871
Tony Stark <vbhat2006@gmail.com>
2026-06-04 16:39:54 +0530

    login endpoint

--- src/main/java/com/example/Attendance_Audit_System/UserController.java
+++ src/main/java/com/example/Attendance_Audit_System/UserController.java
@@ -1,6 +1,7 @@
 package com.example.Attendance_Audit_System;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,9 +20,22 @@ public class UserController
     
     //creating account temporary
     @PostMapping("/login")
-    public Users temp(@Valid @RequestBody Users newUser)//new user contains email and password of the person trying to login
+    public ResponseEntity<String> UserLogin(@Valid @RequestBody Users newUser)//new user contains email and password of the person trying to login
     {
-        if
+        if(!repo.existsByEmail(newUser.getEmail()))// making sure user exists
+        {
+            return  ResponseEntity.badRequest().body("User does not Exist.");
+        }
+
+        Long userid=repo.findIdByEmailAndPassword(newUser.getEmail(), newUser.getPassword());
+
+        if(userid == null) // if use exists by email meaning userid will only be null if password is incorrect
+        {
+            return ResponseEntity.badRequest().body("Invalid password.");
+        }
+
+        return ResponseEntity.ok().body("Login Successful!");
+
     }
 
 }
