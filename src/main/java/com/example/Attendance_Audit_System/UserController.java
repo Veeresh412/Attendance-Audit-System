9a36c5036d0ac0574d247b793fc4d91feca4778a
Tony Stark <vbhat2006@gmail.com>
2026-06-05 12:00:31 +0530

    added login logic

--- src/main/java/com/example/Attendance_Audit_System/UserController.java
+++ src/main/java/com/example/Attendance_Audit_System/UserController.java
@@ -2,40 +2,33 @@ package com.example.Attendance_Audit_System;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.Valid;
-
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins="*")
 public class UserController 
 {
     @Autowired
     UserRepository repo;
-    
-    //creating account temporary
-    @PostMapping("/login")
-    public ResponseEntity<String> UserLogin(@Valid @RequestBody Users newUser)//new user contains email and password of the person trying to login
+
+    @PostMapping
+    public ResponseEntity<String> userLogin(@RequestBody Users newUser)
     {
-        if(!repo.existsByEmail(newUser.getEmail()))// making sure user exists
+        if(!repo.existsByEmail(newUser.getEmail()))
         {
-            return  ResponseEntity.badRequest().body("User does not Exist.");
+            return ResponseEntity.badRequest().body("User does NOT Exist");
         }
 
-        Long userid=repo.findIdByEmailAndPassword(newUser.getEmail(), newUser.getPassword());
 
-        if(userid == null) // if use exists by email meaning userid will only be null if password is incorrect
+        if(repo.findIdByEmailAndPassword(newUser.getEmail(), newUser.getPassword())== null)
         {
-            return ResponseEntity.badRequest().body("Invalid password.");
+            return ResponseEntity.badRequest().body("Incorrect Password"); // if email exists means password is wrong
         }
 
-        return ResponseEntity.ok().body("Login Successful!");
-
+        return ResponseEntity.ok().body("Login Sucessful!");    
     }
 
 }
