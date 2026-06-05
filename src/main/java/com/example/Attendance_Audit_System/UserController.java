e905be7c69c703971d2256c80da7540c23637c17
Tony Stark <vbhat2006@gmail.com>
2026-06-05 12:28:29 +0530

    added dashboard draft

--- src/main/java/com/example/Attendance_Audit_System/UserController.java
+++ src/main/java/com/example/Attendance_Audit_System/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users/login")
+@CrossOrigin(origins="*")
 public class UserController 
 {
     @Autowired
