a0ad44c2f39325d730fa9d146184fb746df7b823
Tony Stark <vbhat2006@gmail.com>
2026-06-09 11:36:33 +0530

    added attendance employee entity class

---
+++ src/main/java/com/example/Attendance_Audit_System/Attendance.java
@@ -0,0 +1,78 @@
+package com.example.Attendance_Audit_System;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="attendance")
+public class Attendance 
+{
+    @Id
+    @Column(name="a_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long aId;
+
+   @ManyToOne
+    // This tells PostgreSQL: "Create a foreign key column named emp_id"
+    @JoinColumn(name = "emp_id", nullable = false)
+    private Employees employee;
+
+    @Column(name="day")
+    private LocalDate workday;
+
+    @Column(name="clock_in")
+    private LocalTime clockIn;
+
+    @Column(name="clock_Out")
+    private LocalTime clockOut;
+
+    // setters and getters
+
+    public Long getAId()
+    {
+        return aId; // no setter since it generates automatically
+    }
+
+    public void setEmployee(Employees employee)
+    {
+        this.employee=employee;
+    }
+    public Employees getEmployee()
+    {
+        return  employee;
+    }
+
+    public void setWorkday(LocalDate workday)
+    {
+        this.workday=workday;
+    }
+    public LocalDate getWorkday()
+    {
+        return workday;
+    }
+    public void setClockIn(LocalTime clockIn)
+    {
+        this.clockIn=clockIn;
+    }
+    public LocalTime getClockIn()
+    {
+        return clockIn;
+    }
+     public void setClockOut(LocalTime clockOut)
+    {
+        this.clockOut=clockOut;
+    }
+    public LocalTime getClockOut()
+    {
+        return clockOut;
+    }
+}
