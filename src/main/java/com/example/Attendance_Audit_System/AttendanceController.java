153185fe2f1c4533722fb9356e08f86ba0f7a6e7
Tony Stark <vbhat2006@gmail.com>
2026-06-10 13:45:05 +0530

    creating rest controller for attendance

---
+++ src/main/java/com/example/Attendance_Audit_System/AttendanceController.java
@@ -0,0 +1,13 @@
+package com.example.Attendance_Audit_System;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/attendance")
+@CrossOrigin(origins = "*") // Allows your frontend to make requests without getting blocked by the browser!
+public class AttendanceController {
+
+ 
+}
--- src/main/java/com/example/Attendance_Audit_System/GlobalExceptionHandler.java
+++ src/main/java/com/example/Attendance_Audit_System/GlobalExceptionHandler.java
@@ -3,12 +3,12 @@ package com.example.Attendance_Audit_System;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.dao.DataIntegrityViolationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
